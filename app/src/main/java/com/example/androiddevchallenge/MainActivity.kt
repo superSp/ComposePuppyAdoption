120efd4ff03d55c3463e31bd2965c0a16f8048e4
616391073@qq.com <616391073@qq.com>
2021-03-01 17:01:54 +0800

    good luck for me

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -23,7 +23,6 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.androiddevchallenge.ui.HomeView
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
